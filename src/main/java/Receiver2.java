b6ee9d956ff977ba4b4b57cb577a16adf3a167df
aidar531 <aidar100497@gmail.com>
2019-10-06 21:00:20 +0300

    Конечный вариант

--- src/main/java/Receiver2.java
+++ src/main/java/Receiver2.java
@@ -5,7 +5,7 @@ import jade.lang.acl.MessageTemplate;
 
 import javax.sound.midi.Receiver;
 
-public class Receiver1 extends Agent
+public class Receiver2 extends Agent
 {
     protected void setup()
     {
@@ -13,7 +13,7 @@ public class Receiver1 extends Agent
         {
             public void action()
             {
-                MessageTemplate template = MessageTemplate.MatchProtocol("P1");
+                MessageTemplate template = MessageTemplate.MatchProtocol("P2");
                 ACLMessage msg = receive(template);
                 if (msg!=null) {
                     System.out.println(" - " +
@@ -21,8 +21,8 @@ public class Receiver1 extends Agent
                             msg.getContent());
                     ACLMessage reply = msg.createReply();
                     reply.setPerformative( ACLMessage.INFORM );
-                    reply.setProtocol("P1");
-                    reply.setContent(" Hi,there " );
+                    reply.setProtocol("P2");
+                    reply.setContent(" go away " );
                     getAgent().send(reply);
                 }
                 block();
--- src/main/java/Sender.java
+++ src/main/java/Sender.java
@@ -1,20 +1,42 @@
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.OneShotBehaviour;
 import jade.lang.acl.ACLMessage;
 
+
 public class Sender extends Agent {
+
+    public static void Sending(String receiver, ACLMessage msg, AID aid, Agent agent) {
+        msg.addReceiver(aid);
+        msg.setContent("Hello, "+ receiver);
+        msg.setProtocol("P1");
+        agent.send(msg);
+        msg.setContent("You are suck, "+ receiver);
+        msg.setProtocol("P2");
+        agent.send(msg);
+    }
     @Override
     protected void setup() {
         addBehaviour(new OneShotBehaviour() {
             @Override
             public void action() {
-                ACLMessage msg1= new ACLMessage(ACLMessage.INFORM), msg2 = new ACLMessage(ACLMessage.INFORM);
-                AID aid = new AID("Receiver1",false);
-                msg1.addReceiver(aid);
-                msg1.setContent("Hello, Smith");
-                msg1.setProtocol("energy");
-                getAgent().send(msg1);
+                ACLMessage msg1_1 = new ACLMessage(ACLMessage.INFORM), msg2_1 = new ACLMessage(ACLMessage.INFORM);
+                AID aid1 = new AID("Receiver1",false); AID aid2 = new AID("Receiver2",false);
+                Sending("Receiver1",msg1_1,aid1,getAgent());
+                Sending("Receiver2",msg2_1,aid2,getAgent());
+            }
+        });
+        addBehaviour(new CyclicBehaviour() {
+            @Override
+            public void action() {
+                ACLMessage msg = receive();
+                if (msg != null) {
+                    System.out.println(" - " +
+                            myAgent.getLocalName() + " получил письмо от " +
+                            msg.getContent());
+                }
+                block();
             }
         });
     }
